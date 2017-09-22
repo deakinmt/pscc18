3f3a71f20fdfe911c06dda955f21649d32d1bc59
Matt Deakin <matthew.deakin@chch.ox.ac.uk>
2017-09-22 18:57:46 +0100

    no message

--- pscc_fncs/run_pscc_feeder.m
+++ pscc_fncs/run_pscc_feeder.m
@@ -2,6 +2,8 @@ function [ RR,figs ] = run_pscc_feeder( FF,pl_options )
 
 % INPUTS TO DETERMINE BEHAVIOUR -------------------
 Vp = FF.Vp; % we set for this network.
+Ip = FF.Ip;
+Pp = FF.Pp;
 NUT = FF.NUT;
 SRC = FF.SRC;
 pg_ssc = FF.pg_ssc;
@@ -21,8 +23,7 @@ DSSSolution.Solve;
 
 Zbus = create_zbus(DSSCircuit);
 
-T_ratio = 1;
-V0 = DSSCircuit.Vsource.pu*T_ratio;
+V0 = DSSCircuit.Vsource.pu;
 
 DSSCircuit.SetActiveBus(NUT);
 vbN = sqrt(3)*DSSCircuit.ActiveBus.kVBase; % LL (@ NUT)
@@ -42,21 +43,21 @@ Z = Z_ohm/zbN; %pu
 
 Ssc = ((vbN^2)/abs(Z*zbN))*1e3; %kW
 
-DSSObj.Text.Command='export elempowers';
-CapQ = calc_capQ(elmpwr_fnm)/sb; %pu 
-LdNom = (calc_load(elmpwr_fnm)/sb); %pu
-Sload = CapQ + LdNom; %pu
+% DSSObj.Text.Command='export elempowers';
+% CapQ = calc_capQ(elmpwr_fnm)/sb; %pu 
+% LdNom = (calc_load(elmpwr_fnm)/sb); %pu
+% Sload = CapQ + LdNom; %pu
+
+Sload = (1769 + 294*1i)/sb;
 
 % create generator at given node:
 DSSText.Command=['new generator.gen phases=3 bus1=',NUT,'.1.2.3 kw=0 pf=0'];
-% DSSText.Command='new monitor.genvi element=generator.gen terminal=1 mode=32 VIPolar=yes';
 DSSText.Command='new monitor.genpq element=generator.gen terminal=1 mode=65 PPolar=no';
 
 %% Run the DSS
 [Pg, Qg] = meshgrid(Ssc*pg_ssc,Ssc*qg_ssc);
 Pf = sign(Qg).*Pg./sqrt(Pg.^2 + Qg.^2);
 
-Pl_mes_lin = zeros(size(Pg));
 totlss = zeros(numel(Pg),2); totpwr = zeros(numel(Pg),2);
 VmaxMes = zeros(numel(Pg),1); VminMes = zeros(numel(Pg),1);
 ImaxMes = zeros(numel(Pg),1);
@@ -78,22 +79,10 @@ for i = 1:numel(Pg)
     totpwr(i,:) = DSSObj.ActiveCircuit.TotalPower/sb; %pu
 end
 toc
-% withdraw results
-% DSSMon=DSSCircuit.Monitors; DSSMon.name='genvi';
-% DSSText.Command='show monitor genvi'; % this has to be in for some reason?
-% Vgen_lin = ExtractMonitorData(DSSMon,1,vbN*1e3/sqrt(3)); %phase 1
-% Vgen_lin2 = ExtractMonitorData(DSSMon,2,vbN*1e3/sqrt(3)); %phase 2
-% Vgen_lin3 = ExtractMonitorData(DSSMon,3,vbN*1e3/sqrt(3)); %phase 3
-% Vgen1=reshape(Vgen_lin,size(Pg));
-% Vgen2=reshape(Vgen_lin2,size(Pg));
-% Vgen3=reshape(Vgen_lin3,size(Pg));
 
 ImaxMat = reshape(ImaxMes,size(Pg));
 VmaxMat = reshape(VmaxMes,size(Pg));
 VminMat = reshape(VminMes,size(Pg));
-% Vgen = max(cat(3,Vgen1,Vgen2,Vgen3),[],3); %assume voltage limit refers to a maximum phase LN voltage magnitude
-% Vgen = reshape(Vgen_lin,size(Pg));
-Vgen = VmaxMat;
 
 DSSMon=DSSCircuit.Monitors; DSSMon.name='genpq';
 DSSText.Command='show monitor genpq';
@@ -103,65 +92,78 @@ Qgen_lin = -ExtractMonitorData(DSSMon,2,1)/sb; %pu, +ve is generation
 TotLss = reshape(totlss(:,1),size(Pg)) + 1i*reshape(totlss(:,2),size(Pg));% +ve implies losses
 TotPwr = reshape(totpwr(:,1),size(Pg)) + 1i*reshape(totpwr(:,2),size(Pg)); %-ve implies load
 
-% calculate maximum power export s.t. voltage constraint:
-% NaN_outs = 0./(Vgen<Vp);
-% Vg_in = Vgen + NaN_outs; % remove numbers where the voltage is too high
-% Vg_in_mn = (Vg_in==max(Vg_in));
-NaN_outs = 0./(VmaxMat<Vp);
-Vg_in = VmaxMat + NaN_outs; % remove numbers where the voltage is too high
-Vg_in_mn = (Vg_in==max(Vg_in));
-max_pwr = max(real(TotPwr(Vg_in_mn)),[],2); % for each row, find the max power
-Vmn = find(real(TotPwr)==repmat(max_pwr',numel(TotPwr)/numel(max_pwr),1)); %find the index of these points
-
 Pgenmat=reshape(Pgen_lin,size(Pg));% - real(TotLd);
 Qgenmat=reshape(Qgen_lin,size(Pg));% - imag(TotLd);
 Sgenmat = Pgenmat+1i*Qgenmat;
 
-Vg2_l= V2_Sl_calc( Sgenmat - Sload,Z,V0,'p' );
-Vg_l = sqrt(Vg2_l);
+Vg2= V2_Sl_calc(Sgenmat - Sload,Z,V0,'p' );
+Vg = sqrt(Vg2);
+%%
+% calculate maximum power export s.t. voltage constraint:
+VNaN_outs = 0./(VmaxMat<Vp);
+Vg_inV = VmaxMat + VNaN_outs; % remove numbers where the voltage is too high
+Vmn = (Vg_inV==max(Vg_inV));
+Vmax_pwr = max(real(TotPwr(Vmn)),[],2); % for each row, find the max power
+PgenV = Pgenmat(Vmn);
 
-Pgen = Pgenmat(Vmn);
+% Vmax_pwr = max(real(TotPwr(Vg_in_mnV)),[],2); % for each row, find the max power
+% Vmn = find(real(TotPwr)==repmat(Vmax_pwr',numel(TotPwr)/numel(Vmax_pwr),1)); %find the index of these points
+% PgenV = Pgenmat(Vmn);
 
-[S0,Sg,Sl,~,Ipr,P0,Q0] = pred_S0_pscc( Pgen, Sload, Z, V0, Vp  );
+% calculate maximum power export s.t. current:
+INaN_outs = 0./((ImaxMat<Ip).*(real(TotPwr)<Pp).*(VmaxMat<Vp).*(VmaxMat>1.05)) ;
+Vg_inI = VmaxMat + INaN_outs + VNaN_outs; % remove high voltages and currents
+
+Imn = (Vg_inI==max(Vg_inI));
+Imax_pwr = max(real(TotPwr(Imn)),[],2); % for each row, find the max power
+% PgenI = Pgenmat(Imn);
+
+
+[S0,Sg,Sl,~,Iest_pu,P0,Q0] = pred_S0_pscc( PgenV, Sload, Z, V0, Vp  );
+Iest = Iest_pu*ibN;
+Iest_in = ((Iest<Ip).*(P0<Pp));
 %%
 % Results table
-TotPwr_max = max(max_pwr);
-P0_l_mx = max(P0);
-RR.P0 = [TotPwr_max;P0_l_mx];
+TotPwr_maxV = max(Vmax_pwr);
+P0_mxV = max(P0);
+RR.P0V = [TotPwr_maxV;P0_mxV];
+Pgen_measV = Pgenmat(real(TotPwr)==TotPwr_maxV);
+Pgen_mxV = real(Sg(P0_mxV==P0) + Sload);
+RR.PgenV = [Pgen_measV;Pgen_mxV];
+Qgen_measV = Qgenmat(real(TotPwr)==TotPwr_maxV);
+Qgen_mxV = imag(Sg(P0_mxV==P0) + Sload);
+RR.QgenV = [Qgen_measV;Qgen_mxV];
+Q0_measV = imag(TotPwr(real(TotPwr)==TotPwr_maxV));
+Q0_mxV = Q0(P0_mxV==P0);
+RR.Q0V = [Q0_measV;Q0_mxV];
 
-Pgen_meas = Pgenmat(real(TotPwr)==TotPwr_max);
-Pgen_l_mx = real(Sg(P0_l_mx==P0) + Sload);
-RR.Pgen = [Pgen_meas;Pgen_l_mx];
+TotPwr_maxI = Imax_pwr(end);
+% P0_I = P0(Iest_in); %remove overcurrents
+P0_mxI = P0(find(Iest_in,1,'last'));
+RR.P0I = [TotPwr_maxI;P0_mxI];
+Pgen_measI = Pgenmat(real(TotPwr)==TotPwr_maxI);
+Pgen_mxI = real(Sg(P0_mxI==P0) + Sload);
+RR.PgenI = [Pgen_measI;Pgen_mxI];
 
-Qgen_meas = Qgenmat(real(TotPwr)==TotPwr_max);
-Qgen_l_mx = imag(Sg(P0_l_mx==P0) + Sload);
-RR.Qgen = [Qgen_meas;Qgen_l_mx];
 
-Q0_meas = imag(TotPwr(real(TotPwr)==TotPwr_max));
-Q0_l_mx = Q0(P0_l_mx==P0);
-RR.Q0 = [Q0_meas;Q0_l_mx];
+% RR.SgenV = RR.PgenV + 1i*RR.QgenV;
+% RR.SgV = Sg(P0_mxV==P0);
+% RR.S0V = RR.P0V + 1i*RR.Q0V;
 
-Imax_mes = ImaxMat(real(TotPwr)==TotPwr_max);
-Imax_prd = Ipr(P0_l_mx==P0)*ibN;
+Imax_mes = ImaxMat(real(TotPwr)==TotPwr_maxV);
+Imax_prd = Iest(P0_mxV==P0)*ibN;
 RR.Imax = [Imax_mes;Imax_prd];
-
-RR.Sgen = RR.Pgen + 1i*RR.Qgen;
-RR.Sg = Sg(P0_l_mx==P0);
-RR.S0 = RR.P0 + 1i*RR.Q0;
 
 RR.FF = FF;
 
-RR.T_ratio = T_ratio;
 RR.sbase = sb;
 RR.Z = Z;
+RR.Zabs = abs(Z);
+RR.lz = lambdas(Z);
 RR.V0 = V0;
-RR.Vp = Vp;
 RR.Ssc = Ssc; %kW
 RR.Sload = Sload; %in pu
 
-Vl_kst = real(Z*Sg(P0_l_mx==P0));
-% RR.dV_ldc = [Vg_max - V0; Vn_ldc - 1; Vl_ldc - 1; Vw_ldc - 1];
-RR.dV_kst = [Vp - V0; Vl_kst - 1];
 %% pl_options - to look through the data and compare methods etc.
 figs = [];
 % possible: check v_comparison pgp0 pgqgq0 plosses qlosses p_d_losses
@@ -169,56 +171,55 @@ figs = [];
 
 if sum(ismember(pl_options,'check')) || sum(ismember(pl_options,'0'))
     figs = [figs;figure('Color','White')];
-    % Seems to work! :DDDDD
     cc=contourf(Pgenmat,Qgenmat,real(TotPwr)); hold on;
     clabel(cc); grid on;
-    contour(Pgenmat,Qgenmat,Vgen*0.01,0.01*(1.0:0.06:1.06),'LineColor','r'); hold on;
+    contour(Pgenmat,Qgenmat,VmaxMat*0.01,0.01*(1.0:0.06:1.06),'LineColor','r'); hold on;
     axis equal;
 end
 if sum(ismember(pl_options,'v_comparison')) || sum(ismember(pl_options,'0'))
     figs = [figs;figure('Color','White')];
     subplot(221)
-    [cc,~]=contourf(Pgenmat,Qgenmat,Vgen);
+    [cc,~]=contourf(Pgenmat,Qgenmat,VmaxMat);
     clabel(cc); axis equal;
     subplot(222)
-    [cc,~]=contourf(Pgenmat,Qgenmat,Vg_l);
+    [cc,~]=contourf(Pgenmat,Qgenmat,Vg);
     clabel(cc); axis equal;
     
     subplot(224)
-    [cc,~]=contourf(Pgenmat,Qgenmat,100*(Vg_l-Vgen));
+    [cc,~]=contourf(Pgenmat,Qgenmat,100*(Vg-VmaxMat));
     clabel(cc); %axis equal;
 end
 if sum(ismember(pl_options,'pgp0'))  || sum(ismember(pl_options,'0'))
-    figs = [figs;figure('Color','White','Position',fig_nompos)];%,'defaultaxesfontsize',12,'defaulttextinterpreter','latex',fig_nompos);
+    figs = [figs;figure('Color','White','Position',fig_nompos,'defaultaxesfontsize',12,'defaulttextinterpreter','latex')];
     
     set(figs,'defaulttextinterpreter','latex');
     set(figs,'defaultaxesfontsize',12);
 
-    plot(Pgenmat(Vmn),max_pwr,'x-'); grid on; hold on;
-    plot(Pgen,P0);
+    plot(Pgenmat(Vmn),Vmax_pwr,'x-'); grid on; hold on;
+    plot(PgenV,P0);
 
     xlabel('$P_{g}$ (pu)');
     ylabel('$P_{0}$ (pu)');
-%     lgnd=legend('Measured','No Load','Full Load','Weighted Load','Location','SouthEast');
-%     set(lgnd,'interpreter','latex');
-    axis equal; %axis([-0.002 0.050 -0.020 0.007]);
+    lgnd=legend('Measured','2 bus P-V curve','Location','SouthEast');
+    set(lgnd,'interpreter','latex');
+    axis equal;
 end
 if sum(ismember(pl_options,'pgqgq0')) || sum(ismember(pl_options,'0'))
-    figs = [figs;figure('Color','White')];
-    plot(Pgenmat(Vmn),Qgenmat(Vmn),'k'); hold on; grid on;
-    plot(Pgen,imag(Sg + Sload),'k:');
-    plot(Pgenmat(Vmn),imag(TotPwr(Vmn)),'r'); hold on;
-    plot(Pgen,Q0,'r:');
+    figs = [figs;figure('Color','White','Position',fig_nompos,'defaultaxesfontsize',12,'defaulttextinterpreter','latex')];
+    plot(Pgenmat(Vmn),Qgenmat(Vmn),'x-','Color',[0.3 0.3 0.3]); hold on; grid on;
+    plot(PgenV,imag(Sg + Sload),'k');
+    plot(Pgenmat(Vmn),imag(TotPwr(Vmn)),'x-','Color',[1.0 0.5 0.5]); hold on;
+    plot(PgenV,Q0,'r');
 
-%     legend('Qg meas','Qgw pred','Qgl pred','Q0 meas','Q0w pred','Q0l pred');
-    xlabel('Pg (p.u)'); ylabel('Q (p.u.)');
-    axis equal;
+    lgnd = legend('$Q_{gen}$ msrd','$Q_{gen}$ estd','$Q_{comp}$ msrd','$Q_{comp}$ estd');
+    set(lgnd,'Interpreter','Latex');
+    xlabel('$P_{gen}$ (pu)'); ylabel('$Q$ (pu)'); axis equal;
 end
 if sum(ismember(pl_options,'plosses')) || sum(ismember(pl_options,'0'))
     figs = [figs;figure('Color','White','Position',[300 200 600 600])];
     plot(Pgenmat(Vmn),real(TotLss(Vmn)),'k','LineWidth',2); hold on;
-    plot(Pgen,real(Sl));
-%     legend('Measured losses','N losses','L losses','W losses','Location','NorthWest');
+    plot(PgenV,real(Sl));
+    legend('Measured P losses','Pred P losses','Location','NorthWest');
     xlabel('Pg (pu)');
     ylabel('Pl (pu)');
     axis equal; grid on;
@@ -226,28 +227,10 @@ end
 if sum(ismember(pl_options,'qlosses')) || sum(ismember(pl_options,'0'))
     figs = [figs;figure('Color','White','Position',[300 200 600 600])];
     plot(Pgenmat(Vmn),imag(TotLss(Vmn)),'k','LineWidth',2); hold on;
-    plot(Pgen,imag(Sl));
-%     legend('N losses','L losses','W losses','Location','NorthWest');
+    plot(PgenV,imag(Sl));
+    legend('Meas Q losses','Pred Q losses','Location','NorthWest');
     xlabel('Pg (pu)');
     ylabel('Ql (pu)');
-    axis equal; grid on;
-end
-if sum(ismember(pl_options,'p_d_losses')) || sum(ismember(pl_options,'0'))
-    figs = [figs;figure('Color','White','Position',[300 200 600 600])];
-    TotLssMes = TotLss(Vmn);
-    plot(Pgen,real(TotLssMes-Sl));
-%     legend('N losses','L losses','W losses','Location','NorthWest');
-    xlabel('Pg (pu)');
-    ylabel('Plmes - Plpred (pu)');
-    axis equal; grid on;
-end
-if sum(ismember(pl_options,'q_d_losses')) || sum(ismember(pl_options,'0'))
-    figs = [figs;figure('Color','White','Position',[300 200 600 600])];
-    TotLssMes = TotLss(Vmn);
-    plot(Pgen,imag(TotLssMes-Sl));
-%     legend('N losses','L losses','W losses','Location','NorthWest');
-    xlabel('Pg (pu)');
-    ylabel('Qlmes - Qlpred (pu)');
     axis equal; grid on;
 end
 if sum(ismember(pl_options,'pgp0s0')) || sum(ismember(pl_options,'0'))
@@ -255,24 +238,24 @@ if sum(ismember(pl_options,'pgp0s0')) || sum(ismember(pl_options,'0'))
     set(figs,'defaulttextinterpreter','latex');
     set(figs,'defaultaxesfontsize',12);
 
-    p1 = plot(Pgenmat(Vmn),(max_pwr+real(Sload)),'x-'); grid on;  hold on;
-    p2 = plot(Pgen,(P0+real(Sload))); plot(0,0);plot(0,0);
-    p3 = plot(Pgenmat(Vmn),abs(max_pwr+imag(TotPwr(Vmn))),'x-');
-    p4 = plot(Pgen,abs(S0));
+    p1 = plot(Pgenmat(Vmn),(Vmax_pwr+real(Sload)),'x-'); grid on;  hold on;
+    p2 = plot(PgenV,(P0+real(Sload))); plot(0,0);plot(0,0);
+    p3 = plot(Pgenmat(Vmn),abs(Vmax_pwr+imag(TotPwr(Vmn))),'x-');
+    p4 = plot(PgenV,abs(S0));
     p5 = plot([-0.7 9.4]/(sb*1e-3),[2.5 2.5]/(sb*1e-3),'k--');
     
     axis equal; axis([-1.5 9.9 -1 14]/(sb*1e-3));
     xlabel('$P_{g}$, p.u.'); ylabel('$S_{(\cdot)}$, p.u.'); 
-%     lgnd = legend([p1 p2 p3 p4 p5],'$P_{g}-P_{l}$, measrd.', '$P_{g}-P_{l}$, wghtd.',...
-%             '$|S_{0}|$, measrd.','$|S_{0}|$, wghtd.','$|S_{0}|$ Sbstn. limit','Location','NorthWest');
-%     set(lgnd,'Interpreter','Latex');
+    lgnd = legend([p1 p2 p3 p4 p5],'$P_{g}-P_{l}$, measrd.', '$P_{g}-P_{l}$, wghtd.',...
+            '$|S_{0}|$, measrd.','$|S_{0}|$, wghtd.','$|S_{0}|$ Sbstn. limit','Location','NorthWest');
+    set(lgnd,'Interpreter','Latex');
 end
 if sum(ismember(pl_options,'vmaxmin')) || sum(ismember(pl_options,'0'))
     figs = [figs;figure('Color','White','Position',fig_nompos)];%,'defaultaxesfontsize',12,'defaulttextinterpreter','latex',fig_nompos);
     plot(Pgenmat(Vmn),VmaxMat(Vmn)); grid on; hold on;
-    plot(Pgenmat(Vmn),Vgen(Vmn));
+    plot(Pgenmat(Vmn),VmaxMat(Vmn));
     plot(Pgenmat(Vmn),VminMat(Vmn));
-    plot([min(Pgenmat(Vmn)) max(Pgenmat(Vmn))],[1.06 1.06],'k--');
+    plot([min(Pgenmat(Vmn)) max(Pgenmat(Vmn))],[1 1]*Vp,'k--');
     plot([min(Pgenmat(Vmn)) max(Pgenmat(Vmn))],[0.94 0.94],'k--');
     legend('Max. Circuit Voltage','|Vg|','Min. Circuit Voltage',...
                     'MV Voltage Limits','Location','SouthWest');
@@ -280,12 +263,15 @@ if sum(ismember(pl_options,'vmaxmin')) || sum(ismember(pl_options,'0'))
     ylabel('|V| (pu)');
 end
 if sum(ismember(pl_options,'imax')) || sum(ismember(pl_options,'0'))
-    figs = [figs;figure('Color','White','Position',fig_nompos)];%,'defaultaxesfontsize',12,'defaulttextinterpreter','latex',fig_nompos);
-    plot(Pgenmat(Vmn),ImaxMat(Vmn)); grid on; hold on;
-    plot(Pgenmat(Vmn),ones(nnz(Vmn),1)*FF.Ip);
-    legend('Max. Circuit Current','I+','Location','SouthWest');
-    xlabel('Pgen (pu)');
-    ylabel('|I| (pu)');
+    figs = [figs;figure('Color','White','Position',fig_nompos,'defaultaxesfontsize',12,'defaulttextinterpreter','latex')];%
+    plot(Pgenmat(Vmn),ImaxMat(Vmn),'x-'); grid on; hold on;
+    plot(PgenV,Iest); grid on; hold on;
+    xs = axis;
+    plot(xs(1:2),[1 1]*FF.Ip,'k--');
+    plot([1 1]*Pgen_measV,xs(3:4),'k:');
+    lgnd = legend('Max $I$, meas.','$I_{g}$, estimated','$I_{+}$','$P_{g}''$, meas','Location','NorthWest');
+    set(lgnd,'Interpreter','Latex');
+    xlabel('$P_{gen}$ (pu)'); ylabel('$|I|$ (A)');
 end
 
 
--- run_pscc_34bus.m
+++ run_pscc_34bus.m
@@ -1,16 +1,18 @@
 close all; clear all; clc;
 cd('C:\Users\chri3793\Documents\MATLAB\DPhil\pscc18_mtlb')
 addpath('pscc_fncs');
-fig_loc = [pwd,'\figures\'];
+% fig_loc = [pwd,'\figures\'];
+fig_loc = 'C:\Users\chri3793\Documents\DPhil\pscc18\pscc18_paper\figures\';
 %%
 cd('C:\Users\chri3793\Documents\MATLAB\DPhil\pscc18_mtlb')
 % INPUTS TO DETERMINE BEHAVIOUR -------------------
 F.Vp = 1.06; %pu
-F.Ip = 180;
-% F.pg_ssc = linspace(-0.001,0.2,20);
-% F.qg_ssc = linspace(-0.4,0.03,20);
-F.pg_ssc = linspace(-0.001,0.2,50);
-F.qg_ssc = linspace(-0.4,0.03,100);
+F.Ip = 180; %A
+F.Pp = 1; %pu
+F.pg_ssc = linspace(-0.001,0.2,20);
+F.qg_ssc = linspace(-0.4,0.03,20);
+% F.pg_ssc = linspace(-0.001,0.2,50);
+% F.qg_ssc = linspace(-0.4,0.03,100);
 
 % remain the same:
 F.SRC = 'SOURCEBUS';
@@ -21,20 +23,84 @@ F.feeder = '34bus'; % for calc_ww and T_ratios
 F.tr_buses = {{'888','832'};{'832','852'};{'850','814'};{'800','SOURCEBUS'}};
 F.fig_nompos = [200 150 450 340];
 
+%% First results figure: 834 bus feeder voltage
+figname1 = [fig_loc,'34bus_pgp0'];
+F.NUT = '834';
+F.pg_ssc = linspace(-0.001,0.2,20);
+F.qg_ssc = linspace(-0.4,0.03,20);
+pl_options={'pgp0'};
+[ ~,figs ] = run_pscc_feeder( F,pl_options );
+
+% export_fig(figs,figname);
+% export_fig(figs,[figname,'.pdf']);
+%% First results figure: 834 bus currents
+figname2 = [fig_loc,'34bus_imax'];
+F.NUT = '834';
+F.pg_ssc = linspace(1e-4,0.2,50);
+F.qg_ssc = linspace(-0.4,0.03,100);
+pl_options={'imax'};
+[ ~,figs ] = run_pscc_feeder( F,pl_options );
+% export_fig(figs,figname);
+% export_fig(figs,[figname,'.pdf']);
+%%
+figname3 = [fig_loc,'34bus_pgqgq0'];
+F.NUT = '834';
+F.pg_ssc = linspace(1e-4,0.2,20);
+F.qg_ssc = linspace(-0.4,0.03,20);
+pl_options={'pgqgq0'};
+[ ~,figs ] = run_pscc_feeder( F,pl_options );
+export_fig(figs,figname);
+export_fig(figs,[figname,'.pdf']);
+
 %%
 F.NUT = '834';
-pl_options={'imax'};
-[ RR,figs ] = run_pscc_feeder( F,pl_options );
+F.pg_ssc = linspace(1e-4,0.2,40);
+F.qg_ssc = linspace(-0.4,0.03,400);
+pl_options={'pgp0','pgqgq0','imax'}; %NB the order DOES matter!
+[ ~,figs ] = run_pscc_feeder( F,pl_options );
+for i = 1:numel(pl_options)
+    figname = [fig_loc,'34bus_',pl_options{i}];
+    export_fig(figs(i),figname);
+    export_fig(figs(i),[figname,'.pdf']);
+end
 
 %%
 NUTs = {'812','828','834','848'};
-pl_options={'imax'};
+F.pg_ssc = linspace(1e-4,0.2,40);
+F.qg_ssc = linspace(-0.4,0.03,200);
+pl_options=NaN;
 
 RR = cell(size(NUTs)); figs = RR;
 for i = 1:numel(NUTs)
     F.NUT = NUTs{i};
     [RR{i},figs{i}] = run_pscc_feeder(F,pl_options);
+
 end
+%
+fig = figure('Color','White','Position',[100 150 550 450],'defaultaxesfontsize',12,'defaulttextinterpreter','latex');
+figname = [fig_loc,'34bus_accuracy'];
+
+for i=1:4
+    subplot(2,2,i)
+    bar([RR{i}.PgenV RR{i}.PgenI RR{i}.P0V RR{i}.P0I ]');
+    title(['Bus ',NUTs{i}]); grid on; 
+    axis([0 5 0 9]);
+    
+    lgnd = legend('Msrd.','Estd.');
+    set(lgnd,'Interpreter','Latex');
+
+    Yticks = yticks;
+    for i = 1:numel(Yticks)
+        ytcks{i} = num2str(Yticks(i));
+    end
+    xtcks = {'$P_{gen}''$','$\hat{P}_{gen}$','$P_{0}''$','$\hat{P}_{0}$'};
+%     format_ticks(gca,xtcks,ytcks(1:i),[],[],[],[],[]);
+    format_ticks(gca,xtcks,[],[],[],[],[],[]);
+    ylabel('$P$ (pu)'); 
+end
+
+% export_fig(fig,figname);
+% export_fig(fig,[figname,'.pdf']);
 
 %%
 fig = figure('Color','White');
@@ -65,6 +131,12 @@ title('Ieee 34 nom tap, Sg pred v. meas.');
 figname = [fig_loc,'ieee34_4tests_nom'];
 % export_fig(fig,figname);
 % export_fig(fig,[figname,'.pdf'],'-dpdf');
+
+
+
+
+
+
 
 
 
